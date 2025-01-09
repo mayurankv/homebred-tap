3d82a36484f40cac8f3fa2c2e28e614e04e1a413
mayurankv <mayuran.k.v@gmail.com>
2025-01-09 15:58:36 +0000

    update

---
+++ Formula/kanata-tray-test.rb
@@ -0,0 +1,16 @@
+class KanataTray < Formula
+  version "v0.5.2"
+  sha256 "59fd53b0926ca64fbf59249a93f99cb90e7afdbe64d16dc65931974cdd00570e"
+
+  desc "Menu bar application for controlling kanata and viewing kanata layer state"
+  homepage "https://github.com/rszyma/kanata-tray"
+  url "https://github.com/rszyma/kanata-tray/releases/download/#{version}/kanata-tray-macos"
+  license "GPL-3.0-only"
+  head "https://github.com/rszyma/kanata-tray.git", branch: "main"
+
+  depends_on "kanata"
+
+  def install
+    bin.install "kanata-tray-macos" => "kanata-tray"
+  end
+end
